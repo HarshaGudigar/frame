ef370d57fad1834528171801ee458246b5e586e5
Harsha Gudigar <harshainspire19@gmail.com>
2026-02-15 12:24:15 +0530

    test: add helper functions for setting up test environment, managing in-memory MongoDB, and user registration.

--- backend/tests/helpers.js
+++ backend/tests/helpers.js
@@ -3,7 +3,7 @@
  */
 const mongoose = require('mongoose');
 const { MongoMemoryServer } = require('mongodb-memory-server');
-const { createApp } = require('../server');
+const { createApp } = require('../app');
 
 let mongod;
 
